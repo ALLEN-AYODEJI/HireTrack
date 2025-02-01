5895e6b91bcde2479feaf0842c3ee825d29a36b7
ALLEN-AYODEJI <allenzeereeh@gmail.com>
2025-02-01 03:34:43 +0100

    COntinuation on the development on the Hero and Landing Page

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,26 +1,45 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Button } from "./ui/button";
 import Image from "next/image";
 
 const HeroSection = () => {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <section className="w-full pt-36 md:pt-48 pb-10">
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
-          <h1>
+          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title">
             Your AI Career Coach for
             <br />
             Professional Success
           </h1>
-          <p>
+          <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl]">
             Harness the power of AI to drive growth, with personalized guidance,
             expert interview preparation, and actionable insights, empowering
             you to achieve lasting job success.
           </p>
         </div>
 
-        <div>
+        <div className="flex justify-center space-x-4">
           <Link href="/dashboard">
             <Button size="lg" className="px-8">
               Get Started
@@ -32,10 +51,10 @@ const HeroSection = () => {
             </Button>
           </Link>
         </div>
-        <div>
-          <div>
+        <div className="hero-image-wrapper mt-5 md:mt-0">
+          <div ref={imageRef} className="hero-image">
             <Image
-              src={"/banner.png"}
+              src={"/banner1.jpg"}
               alt="HireTrack Preview"
               width={1280}
               height={720}
